f871a35fac1c01de009adce7319f3fd8a6b38361
Robert Smith <quad@symbo1ics.com>
2015-04-01 11:03:45 -0700

    Add some preliminary exports.

--- lisp/package.lisp
+++ lisp/package.lisp
@@ -3,5 +3,29 @@
 ;;;; Copyright (c) 2015 Andrey Smirnov & Robert Smith
 
 (defpackage #:cl-iio
-  (:use #:cl))
+  (:use #:cl)
+  ;; cl-iio.lisp
+  (:export
+   #:channel                            ; CLASS
+   #:enable-channel                     ; GENERIC, METHOD
+   #:disable-channel                    ; GENERIC, METHOD
+   #:channel-enabled-p                  ; GENERIC, METHOD
 
+   #:device                             ; CLASS
+   #:device-channels                    ; ACCESSOR
+   #:enable-buffer                      ; GENERIC, METHOD
+   #:disable-buffer                     ; GENERIC, METHOD
+   #:buffer-enabled-p                   ; GENERIC, METHOD
+   #:buffer-length                      ; GENERIC, METHOD, SETF
+   #:set-buffer-length                  ; GENERIC, METHOD
+   #:open-device                        ; GENERIC, METHOD
+   #:close-device                       ; GENERIC, METHOD
+   #:with-open-device                   ; MACRO
+
+   #:find-device-by-name                ; FUNCTION
+   #:find-channel-by-name               ; FUNCTION
+
+   #:read-raw-record                    ; FUNCTION
+   #:parse-raw-record                   ; FUNCTION
+   )
+  )
